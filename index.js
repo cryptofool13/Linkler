dbb87d5ee3dad3140ec03b2f7932bd90f17661e2
mike <fender18295@gmail.com>
2019-05-08 15:22:37 -0400

    crud works

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const db = require("./db");
 const server = express();
 
 server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }));
 server.set("view engine", "pug");
 server.set("views", path.join(__dirname, "views"));
 
@@ -18,6 +19,23 @@ server.get("/home", (req, res) => {
   db.getLinks(links => {
     res.render("index", { links: links });
   });
+});
+
+server.post("/links/new", (req, res) => {
+  let data = req.body;
+  if (data.name && data.url) {
+    db.addLink(data.name, data.url);
+    res.redirect("/home");
+  }
+  // console.log(data);
+  // if(data.hasOwnProperty('name'))
+});
+server.post("/links/del", (req, res) => {
+  // console.log(Object.keys(req.body));
+  for (let name of Object.keys(req.body)) {
+    db.deleteLink(name);
+  }
+  res.redirect("/home");
 });
 
 server.listen(3000, () => {
